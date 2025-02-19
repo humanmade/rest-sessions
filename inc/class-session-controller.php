d679efbcd38d8ce560985aafc59aff7b6f6bdedb
Ryan McCue <me@ryanmccue.info>
2025-02-19 17:47:04 +0000

    Remove commented code

--- inc/class-session-controller.php
+++ inc/class-session-controller.php
@@ -221,9 +221,7 @@ class Session_Controller extends WP_REST_Controller {
 
 		// If the 2FA plugin is active, validate the 2fa part of the request.
 		$valid_2fa = $this->validate_2fa( $request, $user );
-
 		if ( is_wp_error( $valid_2fa ) ) {
-			// wp_clear_auth_cookie();
 			return $valid_2fa;
 		}
 
